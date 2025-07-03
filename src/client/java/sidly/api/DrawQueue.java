5ae3eec5f881d58650a85db7c25b476d4edb5374
SidOfThe7Cs <Etphonehome7272@gmail.com>
2025-07-03 12:59:51 -0500

    drawqueue

---
+++ src/client/java/sidly/api/DrawQueue.java
@@ -0,0 +1,25 @@
+package sidly.api;
+
+import net.minecraft.client.gui.DrawContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class DrawQueue {
+    private static final List<Consumer<DrawContext>> queue = new ArrayList<>();
+
+    public static void add(Consumer<DrawContext> drawCall) {
+        queue.add(drawCall);
+    }
+
+    public static void render(DrawContext context) {
+        for (Consumer<DrawContext> drawCall : queue) {
+            drawCall.accept(context);
+        }
+        queue.clear();
+    }
+
+    public static void clear() {
+        queue.clear();
+    }
+}
--- src/client/java/sidly/api/SidlysApiModClient.java
+++ src/client/java/sidly/api/SidlysApiModClient.java
@@ -2,6 +2,12 @@ package sidly.api;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderTickCounter;
 import sidly.api.Config.Config;
 
 public class SidlysApiModClient implements ClientModInitializer {
@@ -15,5 +21,19 @@ public class SidlysApiModClient implements ClientModInitializer {
 			Config.HANDLER.save();
 		}));
 
+		HudLayerRegistrationCallback.EVENT.register(layeredDrawer -> layeredDrawer.attachLayerBefore(IdentifiedLayer.CHAT, Utils.MY_HUD_LAYER, this::drawToHud));
+		ClientTickEvents.END_CLIENT_TICK.register(this::onClientTick);
+		ClientTickEvents.START_CLIENT_TICK.register(this::onStartClientTick);
+	}
+
+	private void onStartClientTick(MinecraftClient client) {
+		DrawQueue.clear();
+	}
+
+	private void onClientTick(MinecraftClient client) {
+	}
+
+	private void drawToHud(DrawContext context, RenderTickCounter renderTickCounter) {
+		DrawQueue.render(context);
 	}
 }
